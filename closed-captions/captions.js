c8a2f66724205e5ae63995479bc6e61e29e46f7f
Christian van Drunen <christian.van.drunen@hva.nl>
2025-04-08 15:56:53 +0200

    horrible test

--- closed-captions/captions.js
+++ closed-captions/captions.js
@@ -2,67 +2,71 @@ captions[0] = ['0','3'];
 captions[1] = ['2.8','10'];
 captions[2] = ['10.8','12.5'];
 	//And blood-black nothingness began to spin
-captions[3] = ['12.5','15.6'];
-captions[4] = ['17.369','20'];
-captions[5] = ['20.1','23.3'];
+captions[3] = ['10','11'];
+captions[4] = ['10','11'];
+captions[5] = ['10','11'];
 	//Fuck off, skin-job!
-captions[6] = ['23.3','25.2'];
-	
-captions[7] = ['26.8','31.6'];
-captions[8] = ['32.7','33'];
-captions[9] = ['33.2','34'];
-	//Have you ever been in an institution?
-captions[10] = ['34','36'];
-captions[11] = ['36','36.3'];
-captions[12] = ['36.5','37.3'];
-	//Do they keep you in a cell?
-captions[13] = ['37.3','38.6'];
-captions[14] = ['38.6','38.9'];
-captions[15] = ['39.1','39.9'];
-	//When you're not performing your duties, do they keep you in a little box?
-captions[16] = ['39.9','42.8'];
-captions[17] = ['42.8','43.1'];
-captions[18] = ['43.3','44.1'];
-	//Interlinked
-captions[19] = ['44','44.5'];
-captions[20] = ['45','45.7'];
-	//What's it like to hold the hand of someone you love?
-captions[21] = ['45.7','47.6'];
-captions[22] = ['47.6','48.1'];
-captions[23] = ['48.3','49'];
-	//Did they teach you how to feel, finger to finger?
-captions[24] = ['49.1','50.9'];
-captions[25] = ['50.9','51.4'];
-captions[26] = ['51.6','52.3'];
-	//Do you long for having your heart interlinked?
-captions[27] = ['52.3','54'];
-captions[28] = ['54','54.5'];
-captions[29] = ['54.7','55.4'];
-	//Do you dream about being interlinked?
-captions[30] = ['55.4','57.2'];
-captions[31] = ['57.2','58'];
-	//What's it like to hold your child in your arms?
-captions[32] = ['58','59.8'];
-captions[33] = ['59.8','60.3'];
-captions[34] = ['60.3','61'];
-	//Do you feel that there's a part of you that's missing?
-captions[35] = ['61','62.7'];
-captions[36] = ['62.7','63.2'];
-captions[37] = ['63.2','63.9'];
-	//Within cells interlinked.
-captions[38] = ['63.9','65.2'];
-captions[39] = ['65.2','66.6'];
-	//Why don't you say that three times: "within cells interlinked".
-captions[40] = ['66.6','69.1'];
+captions[6] = ['10','11'];
 
-captions[41] = ['69.1','70.6'];
-captions[42] = ['70.6','72.2'];
-captions[43] = ['72.2','74.2'];
+captions[7] = ['10','11'];
+captions[8] = ['10','11'];
+captions[9] = ['10','11'];
+	//Have you ever been in an institution?
+captions[10] = ['10','11'];
+captions[11] = ['10','11'];
+captions[12] = ['10','11'];
+	//Do they keep you in a cell?
+captions[13] = ['10','11'];
+captions[14] = ['10','11'];
+captions[15] = ['10','11'];
+	
+// aliens appear
+captions[16] = ['30.9','33.8'];
+captions[17] = ['33.9','55.6'];
+captions[18] = ['55.7','60.0'];
+captions[19] = ['60','64.7'];
+
+
+	//big alien
+captions[20] = ['64.7','66.1'];
+captions[21] = ['67.6','68.0'];
+captions[22] = ['71.0','72.2'];
+captions[23] = ['73.3','74.0'];
+
+//Did they teach you how to feel, finger to finger?
+captions[24] = ['120','121'];
+captions[25] = ['120','121'];
+captions[26] = ['120','121'];
+	//Do you long for having your heart interlinked?
+captions[27] = ['120','121'];
+captions[28] = ['120','121'];
+captions[29] = ['120','121'];
+	//Do you dream about being interlinked?
+captions[30] = ['120','121'];
+captions[31] = ['120','121'];
+	//What's it like to hold your child in your arms?
+captions[32] = ['120','121'];
+captions[33] = ['120','121'];
+captions[34] = ['120','121'];
+	//Do you feel that there's a part of you that's missing?
+captions[35] = ['120','121'];
+captions[36] = ['120','121'];
+captions[37] = ['120','121'];
+	//Within cells interlinked.
+captions[38] = ['120','121'];
+captions[39] = ['120','121'];
+	//Why don't you say that three times: "within cells interlinked".
+captions[40] = ['120','121'];
+
+captions[41] = ['120','121'];
+captions[42] = ['120','121'];
+captions[43] = ['120','121'];
 	// We're done.
-captions[44] = ['77.1','78.7'];
-captions[45] = ['80','81.738'];
-captions[46] = ['82.2','84.3'];
-captions[47] = ['85.4','87'];
+captions[44] = ['120','121'];
+captions[45] = ['120','121'];
+captions[46] = ['120','121'];
+captions[47] = ['120','121'];
 
 // //end manual labour
-captions[48] = ['87.68','88.18'];
+captions[48] = ['120','121'];
+
